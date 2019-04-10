a437c9952df33dc135de5c842c43ae69f4ee7632
zhujiayan <zhujiayan1989@sina.cn>
2019-04-10 14:31:36 +0800

    zhujiayan

--- roadnet_embedding.sh
+++ roadnet_embedding.sh
@@ -3,5 +3,5 @@ inputfile=/nfs/project/zhujy/roadnet/mm_nextlink.txt.20180719
 formatfile=road_graph/roadnet.adjlist
 awk -F "\t" '{gsub(","," ",$2);print $1" "$2}' ${inputfile} > ${formatfile}
 deepwalk --format adjlist --input ${formatfile} \
---max-memory-data-size 0 --number-walks 80 --representation-size 128 --walk-length 40 --window-size 10 \
---workers 20 --output road_graph/roadnet.embeddings --undirected ""
+--max-memory-data-size 10000000000 --number-walks 80 --representation-size 128 --walk-length 40 --window-size 10 \
+--workers 80 --output road_graph/roadnet.embeddings --undirected ""
