38851e85dd147a38b4655504fc8644d31c8d431a
zhujiayan <zhujiayan1989@sina.cn>
2019-04-15 15:36:26 +0800

    zhujiayan

--- embedding_evaluate.sh
+++ embedding_evaluate.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export PYTHONPATH=$PYTHONPATH:.
 formatfile=road_graph/roadnet.adjlist
 embfile=/nfs/project/zhujy/roadnet/output/roadnet.embeddings
 python -u example_graphs/scoring_new.py --emb ${embfile} \
