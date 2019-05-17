63dfaad580d6540789633c0f53647ddff03bb094
zhujiayan <zhujiayan1989@sina.cn>
2019-05-17 17:33:06 +0800

    zhujiayan

--- als_embedding.sh
+++ als_embedding.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 export PYTHONPATH=$PYTHONPATH:.
 #inputfile=/nfs/project/zhujy/roadnet/mm_nextlink.txt.20180719
-formatfile=road_graph/roadnet.adjlist
-outputfile=roadnet.embeddings
+#formatfile=road_graph/roadnet.adjlist
+#outputfile=roadnet.embeddings
 formatfile=example/karate.adjlist
 outputfile=karate.adjlist.als
-
+dim=64
 python -u als_train.py --format adjlist --input ${formatfile} \
---max-memory-data-size 0 --representation-size 64 \
---output ${outputfile}
+--max-memory-data-size 0 --representation-size {dim} \
+--output ${outputfile}.tmp
 # --undirected ""
+count=`cat ${outputfile}.tmp|wc -l`
+echo "${count} ${dim}" > ${outputfile}
+awk '{print NR" "$0}' ${outputfile}.tmp >> ${outputfile}
+rm -f ${outputfile}.tmp
 
 python -u example_graphs/scoring_new.py --emb ${outputfile} \
 --network ${formatfile}
