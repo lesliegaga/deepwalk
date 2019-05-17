e1519d8fbfde14c88b7ff5c61d8b11c9e05b2cf6
zhujiayan <zhujiayan1989@sina.cn>
2019-05-17 17:23:23 +0800

    zhujiayan

--- als_embedding.sh
+++ als_embedding.sh
@@ -2,6 +2,8 @@
 #inputfile=/nfs/project/zhujy/roadnet/mm_nextlink.txt.20180719
 formatfile=road_graph/roadnet.adjlist
 outputfile=roadnet.embeddings
+formatfile=example/karate.adjlist
+outputfile=karate.adjlist.als
 
 python -u als_train.py --format adjlist --input ${formatfile} \
 --max-memory-data-size 0 --representation-size 64 \
